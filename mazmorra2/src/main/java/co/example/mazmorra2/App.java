d37ac4a1915fb0f6b5b832b9f6fc2d7827ae325b
frank <arkymiller1@gmail.com>
2024-10-24 20:25:04 -0500

    metodo main creado. iniciando clases

---
+++ mazmorra2/src/main/java/co/example/mazmorra2/App.java
@@ -0,0 +1,29 @@
+package co.example.mazmorra2;
+
+import java.lang.Thread;
+
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) throws InterruptedException {
+
+    	System.out.println("-----mazmorra 1------");
+    	Thread.sleep(1000);
+    	System.out.println("hecho por frank");
+    	Thread.sleep(1000);
+    	System.out.println("-----como jugar----");
+    	Thread.sleep(1000);
+    	System.out.println("-------1 avanzar--------");
+    	Thread.sleep(1000);
+    	System.out.println("-------2 retroceder-----");
+    	Thread.sleep(1000);
+    	System.out.println("-------3 derecha--------");
+    	Thread.sleep(1000);
+    	System.out.println("-------4 izquierda-----");
+    	
+    	
+    	
+    }
+}
